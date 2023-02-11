7e7dbd757b29abf6fa63943242a5a3b0ccfce7ee
akazad1235 <akazad914@gmail.com>
2023-02-11 18:31:22 +0600

    admin manage ticket data show using by yajra

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+
 
 class TicketController extends Controller
 {
-    public function tickets (){
+    public function tickets (Request $req){
         $data = Ticket::get();
-        return view('admin.ticket.manage_tickets', compact('data'));
+        if ($req->ajax()) {
+            return DataTables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function($row){
+                    //    $btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm">View</a>';
+                    $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" class="edit btn  btn-danger btn-sm manageOrder">Open Conversion</a>';
+
+                    //  $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deletePost">Delete</a>';
+                    return $btn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+        return view('admin.ticket.manage_tickets',compact('data'));
     }
 }
--- resources/views/customer/generate_ticket.blade.php
+++ resources/views/customer/generate_ticket.blade.php
@@ -75,12 +75,10 @@
                         Select Your Product (not mendetory)
                     </label>
                     <select class="form-control @error('product_id') is-invalid @enderror" id="product" name="product_id">
-                        <option value="sssssss">Select One</option>
-                        <option value="sssssxxxss">Select Once</option>
-                        {{-- @foreach ($products as $product)
+                        <option value="">Select One</option>
+                        @foreach ($products as $product)
                             <option value="{{ $product->id }}">{{ $product->name }} </option>
-                        @endforeach --}}
-
+                        @endforeach
                     </select>
                     @error('product_id')
                         <span class="invalid-feedback" role="alert">
